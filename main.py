5e3ad295bbccfba232c78534eb6a9cd5938e0477
Ayshwarya Jagadeesan <ayshjag@msla0669.attlocal.net>
2022-12-06 12:31:13 -0500

    removed everything-just deploying the app on heroku

--- main.py
+++ main.py
@@ -24,23 +24,23 @@ result=" "
 async def startup_event():
     
     
-    try:
-        f1=open("./startup/spek_message_id.json")
-        message_code= json.load(f1)
-        global start_up_code
-        start_up_code["message_code"]=message_code
-        #print(message_code)
-        f2=open("./startup/templates.json")
-        templates= json.load(f2)
-        start_up_code["templates"]=templates
-        items["foo"] = {"startup": "Complete"}
-        print("startup complete")
-        #global result 
-        #result= json.dumps(start_up_code)
-        #print(result)
-    except Exception as e:
-        print("Looks like there is some problem in connection,see below traceback")
-        raise e
+    # try:
+    #     f1=open("./startup/spek_message_id.json")
+    #     message_code= json.load(f1)
+    #     global start_up_code
+    #     start_up_code["message_code"]=message_code
+    #     #print(message_code)
+    #     f2=open("./startup/templates.json")
+    #     templates= json.load(f2)
+    #     start_up_code["templates"]=templates
+    #     items["foo"] = {"startup": "Complete"}
+    print("startup complete")
+    #     #global result 
+    #     #result= json.dumps(start_up_code)
+    #     #print(result)
+    # except Exception as e:
+    #     print("Looks like there is some problem in connection,see below traceback")
+    #     raise e
 
 @app.get("/")
 async def root():
@@ -52,8 +52,8 @@ async def read_items(item_id: str):
 
 @app.post("/getproviderinfo/")
 async def getproviderinfo(info:Request):
-    req_info1 =await info.json()
-    template=start_up_code["templates"]
+    # req_info1 =await info.json()
+    # template=start_up_code["templates"]
     ##Running Candidate Smasher
     # content =json.dumps(req_info1)
     # md_source=json.dumps(template)
@@ -64,17 +64,17 @@ async def getproviderinfo(info:Request):
     # file1.writelines(spek_cs)
     # file1.close()
     ##  running think pudding-dummycode
-    spek_tp= "./tests/es_spek_tp.json"
-    with open(spek_tp, "r") as file_d:
-        spek_tp = json.load(file_d)
+    # spek_tp= "./tests/es_spek_tp.json"
+    # with open(spek_tp, "r") as file_d:
+    #     spek_tp = json.load(file_d)
     ##Running Mod Collector
     # spek_tp = req_info1
-    performance_data = req_info1["Performance_data"]
-    performance_data_df =pd.DataFrame (performance_data, columns = [ "Staff_Number","Measure_Name","Month","Passed_Count","Flagged_Count","Denominator","Peer_Average"])
-    performance_data_df.columns = performance_data_df.iloc[0]
-    performance_data_df = performance_data_df[1:]
-    measure_list=[]
-    measure_list=performance_data_df['Measure_Name'].drop_duplicates()
+    # performance_data = req_info1["Performance_data"]
+    # performance_data_df =pd.DataFrame (performance_data, columns = [ "Staff_Number","Measure_Name","Month","Passed_Count","Flagged_Count","Denominator","Peer_Average"])
+    # performance_data_df.columns = performance_data_df.iloc[0]
+    # performance_data_df = performance_data_df[1:]
+    # measure_list=[]
+    # measure_list=performance_data_df['Measure_Name'].drop_duplicates()
     
     # mc=mod_collector.Mod_collector(spek_tp,performance_data_df)
     # mc.transform()
@@ -83,9 +83,9 @@ async def getproviderinfo(info:Request):
     # mc.trend_pred()
     # spek_mc= mc.mod_collector_output()
     # ##Running Esteemer
-    preferences = req_info1['Preferences']
-    history=req_info1['History']
-    message_code =start_up_code['message_code']
+    # preferences = req_info1['Preferences']
+    # history=req_info1['History']
+    # message_code =start_up_code['message_code']
     # es = esteemer.Esteemer(measure_list,spek_tp,preferences,message_code,history,performance_data_df)
     # es.transform()
     # es.apply_preferences(preferences)
